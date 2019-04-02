4d166044d4f148f51f32851e2eb311fe6ca16c32
iambipika <bpka24@hotmail.com>
2019-04-02 15:30:49 +0300

    Service, Mediator and ontology and RDG added
    Service, Mediator and update ontology and RDG added

---
+++ bookingService/src/main/java/fi/jyu/Server/SSWAPServlet.java
@@ -0,0 +1,48 @@
+package fi.jyu.Server;
+
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
+import info.sswap.api.servlet.SimpleSSWAPServlet;
+import fi.jyu.Server.SSWAPService;
+
+/**
+ * Servlet implementation class SSWAPServlet
+ */
+@WebServlet("/SSWAPServlet")
+public class SSWAPServlet extends SimpleSSWAPServlet {
+private static final long serialVersionUID = 1L;
+    
+	@Override
+	public void init(ServletConfig servletConfig) throws ServletException {
+		// always do this
+		
+		
+		
+		super.init(servletConfig);
+
+		// do anything else here that needs to be done once, on servlet load
+
+		// if needed, to increase the timeout above the default 2 mins
+		//int timeout = 10 * 60 * 1000;	// 5 mins in milliseconds
+		//setTimeout(timeout);
+	}
+
+	/*
+	 * We can override this method to connect this servlet with the
+	 * service class, or define the init-param 'ServiceClass' in
+	 * web.xml
+	 * 
+	 * @see info.sswap.api.servlet.SimpleSSWAPServlet#getServiceClass()
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T> Class<T> getServiceClass() {
+
+		return (Class<T>) SSWAPService.class;
+	}
+
+}
+
